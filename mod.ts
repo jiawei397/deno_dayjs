e6a8c82d0337808e4b4eec1755042c81484fbe44
jw397 <jw397@126.com>
2022-03-23 10:23:28 +0800

    chore: update dayjs

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 // @deno-types="./index.d.ts"
-import dayjs from "https://cdn.skypack.dev/pin/dayjs@v1.10.7-Fs0gULP3uteC7O0DDOOk/mode=imports/optimized/dayjs.js";
+import dayjs from "https://cdn.skypack.dev/pin/dayjs@v1.11.0-Ea3wQFUgDJosOLjvHoCa/mode=imports/optimized/dayjs.js";
 
 export default dayjs;
